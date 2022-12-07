ea84fd23b0b918a5a21d9fe652d14643534c3c7e
Фукиш <egor158741@yandex.ru>
2022-12-07 22:13:49 +0300

    Syncword trimming and minor fix

--- Src/TCP-CAS-9_Image_Decoder.py
+++ Src/TCP-CAS-9_Image_Decoder.py
@@ -13,7 +13,7 @@ try:
 except socket.error:
 	print('Failed to create socket')
 	sys.exit()
-s.settimeout(1)
+s.settimeout(0.458)
 imgfr = ""
 host = ip
 try:
@@ -22,48 +22,40 @@ except socket.gaierror:
 	print('Hostname could not be resolved. Exiting')
 	sys.exit()
 s.connect((remote_ip , port))
-print("----------------------------------------------")
-print("                                              ")
-print("       XW-3 (CAS-9) Image Frames Reader       ")
-print("                by Egor UB1QBJ                ")
-print("                                              ")
-print("----------------------------------------------")
+print("------------------------------------------------------")
+print("                                                      ")
+print("       XW-3 (CAS-9) Image Frames Reader/Decoder       ")
+print("                    by Egor UB1QBJ                    ")
+print("                                                      ")
+print("------------------------------------------------------")
 print('Connected to ' + str(remote_ip) + ":" + str(port))
 print("")
 print("To stop receiving and saving photos, press the key combination (CTRL + C) in this window!")
+print("")
 if __name__ == "__main__":
     try:
         while True:
             try:
-                reply = s.recv(555)
+                reply = s.recv(1024)
                 imgfr = reply.hex()[:36]
                 if(len(reply.hex()) == 550):
                     with open('image.data', 'ab') as ff:
-                        ff.write(binascii.unhexlify(reply.hex()))
+                        ff.write(binascii.unhexlify(reply.hex()[36:-2]))
                     print("New image frame! line: " + str(n) + " | Frame Sync: 0x" + str(imgfr.upper()), end='\r')
                     n += 1
                 if(len(reply.hex()) != 550):
-                    print("Telemetry frame! Sync: " + imgfr + " Skip...                                     ", end='\r')
+                    print("Telemetry frame! Sync: " + imgfr + " Skip..." + " "*20, end='\r')
             except socket.timeout:
                 continue
     except KeyboardInterrupt:
         with open('image.data', "rb") as image:
             f = image.read()
         size = os.path.getsize('image.data')
-        l = int(size)/int(275)
+        l = int(size)/int(256)
         if(float(l).is_integer() == True):
             bbyteArray = bytearray(f)
-            grayImage = numpy.array(bbyteArray).reshape(int(l), int(275))
-            print("Saving 256 px...")
+            grayImage = numpy.array(bbyteArray).reshape(int(l), int(256))
+            print("Saving 256 px..." + " "*100)
             cv2.imwrite(outfile, grayImage)
-            print("Saved!")
-            sys.exit()
-        else:
-            fl = math.floor(l)
-            normalsize = int(275)*int(fl)
-            cut = int(size-normalsize)
-            bbyteArray = bytearray(f)[:-cut]
-            grayImage = numpy.array(bbyteArray).reshape(int(l), int(275))
-            print("Saving 256 px...")
-            cv2.imwrite(outfile, grayImage)
+            print("Saved!" + " "*100)
             sys.exit()
--- Windows builds/start.bat
+++ Windows builds/start.bat
@@ -1,4 +1,13 @@
 @echo off
+rem Bypass "Terminate Batch Job" prompt.
+if "%~1"=="-FIXED_CTRL_C" (
+   REM Remove the -FIXED_CTRL_C parameter
+   SHIFT
+) ELSE (
+   REM Run the batch with <NUL and -FIXED_CTRL_C
+   CALL <NUL %0 -FIXED_CTRL_C %*
+   GOTO :EOF
+)
 del image.data
 cls
 TCP-CAS-9_Image_Decoder.exe -p 8100 -ip 127.0.0.1 -o out.png
