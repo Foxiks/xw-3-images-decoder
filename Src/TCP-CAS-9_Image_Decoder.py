9045364382785999dade0256ab7b8049fd4df76c
Фукиш <egor158741@yandex.ru>
2022-12-09 00:11:54 +0300

    WIP! Support 512px image mode

--- Src/TCP-CAS-9_Image_Decoder.py
+++ Src/TCP-CAS-9_Image_Decoder.py
@@ -1,4 +1,4 @@
-import binascii, os, argparse, cv2, sys, socket
+import binascii, os, argparse, cv2, sys, socket, math
 import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", help="port")
@@ -17,6 +17,7 @@ except socket.error:
 s.settimeout(0.458)
 imgfr = ""
 host = ip
+mode = 256
 try:
 	remote_ip = socket.gethostbyname( host )
 except socket.gaierror:
@@ -44,8 +45,11 @@ if __name__ == "__main__":
                         ff.write(binascii.unhexlify(reply.hex()[36:-2]))
                     print("New image frame! line: " + str(n) + " | Frame Sync: 0x" + str(imgfr.upper()), end='\r')
                     n += 1
+                    if(n >= 280):
+                        mode = 512
                 if(len(reply.hex()) != 550):
                     print("Telemetry frame! Sync: " + imgfr + " Skip..." + " "*20, end='\r')
+                    mode = 256
             except socket.timeout:
                 continue
     except KeyboardInterrupt:
@@ -63,5 +67,36 @@ if __name__ == "__main__":
             beta = 1.7 # Brightness control (0-100)
             manual_result = cv2.convertScaleAbs(grayImage, alpha=alpha, beta=beta)
             cv2.imwrite(str('normalized_')+outfile, manual_result)
-            print("Saved!" + " "*100)
-            sys.exit()
+            if(mode == 512):
+                l1 = int(size)/int(256*2)
+                if(float(l1).is_integer() == True):
+                    grayImage2 = np.array(bbyteArray).reshape(int(l1), int(256*2))
+                    print("Saving 512 px..." + " "*100)
+                    cv2.imwrite(outfile, grayImage2)
+                    print("Saving 256 px normalized..." + " "*100)
+                    alpha = 1.5 # Contrast control (1.0-3.0)
+                    beta = 1.7 # Brightness control (0-100)
+                    manual_result2 = cv2.convertScaleAbs(grayImage2, alpha=alpha, beta=beta)
+                    cv2.imwrite(str('normalized_')+outfile, manual_result2)
+                    print("Saved!" + " "*100)
+                    sys.exit()
+                else:
+                    fl = math.floor(l1)
+                    normalsize = int(l1)*int(fl)
+                    cut = int(size-normalsize)
+                    print("Converting...")
+                    #print(cut)
+                    bbyteArray = bytearray(f)[:-cut]
+                    grayImage2 = np.array(bbyteArray).reshape(int(l1), int(256*2))
+                    print("Saving 512 px..." + " "*100)
+                    cv2.imwrite(outfile, grayImage2)
+                    print("Saving 256 px normalized..." + " "*100)
+                    alpha = 1.5 # Contrast control (1.0-3.0)
+                    beta = 1.7 # Brightness control (0-100)
+                    manual_result2 = cv2.convertScaleAbs(grayImage2, alpha=alpha, beta=beta)
+                    cv2.imwrite(str('normalized_')+outfile, manual_result2)
+                    print("Saved!" + " "*100)
+                    sys.exit()
+            if(mode == 256):
+                print("Saved!" + " "*100)
+                sys.exit()
